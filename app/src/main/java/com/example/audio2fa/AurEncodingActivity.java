474d9a5d63dcb87dbdc09dd2c82d5d22bf07f2d1
phanijsp <phani.jsp@gmail.com>
2023-09-11 16:08:36 -0500

    Working Audino encoding in android

---
+++ app/src/main/java/com/example/audio2fa/AurEncodingActivity.java
@@ -0,0 +1,14 @@
+package com.example.audio2fa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AurEncodingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aur_encoding);
+    }
+}
---
+++ app/src/main/java/com/example/audio2fa/AurEncodingActivity2.java
@@ -0,0 +1,14 @@
+package com.example.audio2fa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AurEncodingActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aur_encoding2);
+    }
+}
